abfaad08b4339b6bee533bd4cdab7c0bb5cb4a33
Moul l'App <email@gmail.com>
2026-06-16 16:05:52 +0100

    Bump versionCode to 11 to allow updates without losing permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bocadillo.godroidautomator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "3.3"
+        versionCode = 11
+        versionName = "3.4"
     }
 
     buildTypes {
