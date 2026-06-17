c885ee1ff494ac7c89cc45af887ba5e9994e5354
Moul l'App <email@gmail.com>
2026-06-17 02:40:42 +0100

    fix: add missing import for launch

--- app/src/main/java/com/bocadillo/godroidautomator/GoDroidNotificationService.kt
+++ app/src/main/java/com/bocadillo/godroidautomator/GoDroidNotificationService.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
+import kotlinx.coroutines.launch
 
 class GoDroidNotificationService : NotificationListenerService() {
 
